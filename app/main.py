1d97e08a4d52c4d4d89efbcbf9940877d3b4634e
Luan <luan.valentino78@gmail.com>
2025-10-20 22:18:29 -0300

    Commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename:str) -> None:
+        self.filename = filename
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            os.path.exists(self.filename) and os.remove(self.filename)
+        except Exception as e:
+            print(f"Error removing file {self.filename} : {e}")
+            return False
+        return True
